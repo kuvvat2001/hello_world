5346f40981fc9a8054480a629b3ca22cb62a5e39
kuvvat2001 <kuwwatchik09@gmail.com>
2024-01-18 00:23:24 +0300

    kuvvat_2001

--- lib/presentation/question_tests/equation_tests_screen.dart
+++ lib/presentation/question_tests/equation_tests_screen.dart
@@ -132,7 +132,7 @@ class _EquationTestsScreenState extends State<EquationTestsScreen> {
                       )
                   ]),
                 ],
-              )
+              ) 
           ],
         ),
       ),
