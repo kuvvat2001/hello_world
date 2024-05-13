f787a289145c8a7a1494d0d680b8f143de2b05ea
Guvanch Amanov <guvanch.amanov@skystart.team>
2024-05-13 12:54:29 +0300

    statics screen

--- lib/presentation/screen/statistic_screen.dart
+++ lib/presentation/screen/statistic_screen.dart
@@ -1,15 +1,55 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:math_app/data/repository_impl/local__store_repository.dart';
+import 'package:math_app/domain/repository/repository.dart';
+import 'package:math_app/presentation/utils/const.dart';
+import 'package:math_app/presentation/widgets/chart_widget.dart';
 
 class StatisticScreen extends StatefulWidget {
-  const StatisticScreen({super.key});
+  const StatisticScreen({
+    super.key,
+    required this.repository,
+    required this.pref,
+  });
+  final IRepository repository;
+  final LocalStoreRepository pref;
 
   @override
   State<StatisticScreen> createState() => _StatisticScreenState();
 }
 
 class _StatisticScreenState extends State<StatisticScreen> {
+  final List<int> _listValue = [];
+  bool isLoading = false;
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  void init() async {
+    isLoading = true;
+    final data = await widget.repository.getTasks();
+    for (var i = 0; i < data.data.length; i++) {
+      if (widget.pref.checkKey('${AppConstants.kTestQuestion}$i')) {
+        final savedData =
+            widget.pref.getString('${AppConstants.kTestQuestion}$i');
+        final parsed = jsonDecode(savedData) as Map<String, dynamic>;
+        _listValue.add(parsed.values.where((e) => e == true).length);
+      }
+      setState(() {});
+      isLoading = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator.adaptive())
+          : AnalyticsBarChart(items: _listValue),
+    );
   }
 }
---
+++ lib/presentation/widgets/chart_widget.dart
@@ -0,0 +1,155 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class AnalyticsBarChart extends StatelessWidget {
+  const AnalyticsBarChart({super.key, required this.items});
+  final List<int> items;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24).copyWith(bottom: 12),
+            child: const Text(
+              "Test Statistics",
+              style: TextStyle(fontSize: 26),
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 1,
+            child: items.isEmpty
+                ? const Center(child: Text('---'))
+                : Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final barsSpace = 10.0 * constraints.maxWidth / 200;
+                        final barsWidth = 10.0 * constraints.maxWidth / _width;
+                        return BarChart(
+                          BarChartData(
+                            alignment: BarChartAlignment.center,
+                            barTouchData: BarTouchData(enabled: false),
+                            titlesData: FlTitlesData(
+                              show: true,
+                              bottomTitles: AxisTitles(
+                                sideTitles: SideTitles(
+                                  showTitles: true,
+                                  //reservedSize: 28,
+                                  // interval: _interval,
+                                  getTitlesWidget: bottomTitleWidgets,
+                                ),
+                              ),
+                              rightTitles: AxisTitles(
+                                sideTitles: SideTitles(
+                                  showTitles: true,
+                                  reservedSize: 40,
+                                  getTitlesWidget: (value, meta) =>
+                                      rightTitles(value, meta, context),
+                                ),
+                              ),
+                              topTitles: const AxisTitles(
+                                sideTitles: SideTitles(showTitles: false),
+                              ),
+                              leftTitles: const AxisTitles(
+                                sideTitles: SideTitles(showTitles: false),
+                              ),
+                            ),
+                            gridData: FlGridData(
+                              show: true,
+                              verticalInterval: 0.1,
+                              //checkToShowHorizontalLine: (value) => value % 10 == 0,
+                              getDrawingHorizontalLine: (value) => const FlLine(
+                                color: Colors.grey,
+                                strokeWidth: 1,
+                              ),
+                              getDrawingVerticalLine: (value) => const FlLine(
+                                color: Colors.grey,
+                                strokeWidth: 1,
+                              ),
+                              drawHorizontalLine: true,
+                              drawVerticalLine: true,
+                            ),
+                            borderData: FlBorderData(
+                              show: true,
+                              border: const Border(
+                                top: BorderSide(color: Colors.grey),
+                              ),
+                            ),
+                            groupsSpace: barsSpace,
+                            barGroups: List.generate(
+                              items.length,
+                              (i) => makeGroupData(
+                                i,
+                                items[i].toDouble(),
+                                barsWidth,
+                                barsSpace,
+                                context,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ),
+        ],
+      );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    // uytgedip bilersin
+    final s = switch (value.toInt()) {
+      0 => 'Test 1',
+      1 => 'Test 2',
+      2 => 'Test 3',
+      3 => 'Test 4',
+      4 => 'Test 5',
+      5 => 'Test 6',
+      6 => 'Test 7',
+      7 => 'Test 8',
+      8 => 'Test 9',
+      9 => 'Test 10',
+      10 => 'Test 11',
+      11 => 'Test 12',
+      _ => '',
+    };
+    return Text(s, style: const TextStyle(fontSize: 10));
+  }
+
+  Widget rightTitles(double value, TitleMeta meta, BuildContext context) {
+    Widget axisTitles = Text(
+      (num.tryParse(meta.formattedValue) ?? 0) is int
+          ? meta.formattedValue
+          : '',
+    );
+    if (value == meta.max) {
+      final remainder = value % meta.appliedInterval;
+      if (remainder != 0.0 && remainder / meta.appliedInterval < 0.5) {
+        axisTitles = const SizedBox.shrink();
+      }
+    }
+    return SideTitleWidget(axisSide: meta.axisSide, child: axisTitles);
+  }
+
+  double get _width => switch (items.length) {
+        < 7 && > 4 => 150,
+        > 10 => 300,
+        < 4 => 50,
+        _ => 200,
+      };
+
+  BarChartGroupData makeGroupData(int x, double y, double barsWidth,
+          double barsSpace, BuildContext context) =>
+      BarChartGroupData(
+        x: x,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: y,
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
+            width: barsWidth,
+            color: Colors.blueAccent,
+          ),
+        ],
+      );
+}
